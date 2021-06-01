71380d42e7d01241a1ba76987fb9c8707b9c1007
Kienn Thiett <smoothkh4951@gmail.com>
2021-06-01 18:48:45 +0700

    gitnore

---
+++ reactjs/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Redirect, Route } from 'react-router';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
+import main from './main';
+
+
+import './style.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={main} />
+            <Route exact path='/accounts/login' component={Login} />
+            <Route exact path='/accounts/register' component={Register} />
+            <Route exact path='/home' component={Home} />
+
+            {/* <Route component={NotFound}/> */}
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
