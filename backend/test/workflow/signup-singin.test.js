3946655654585de447c763ae4d6dc1c6f981712e
Kienn Thiett <smoothkt4951@gmail.com>
2021-06-29 21:37:22 +0700

    workflow signup - sign in then add product

--- backend/test/workflow/signup-singin.test.js
+++ backend/test/workflow/signup-singin.test.js
@@ -52,22 +52,5 @@ describe('POST /accounts/register', () =>{
             })
         })
     })
-
-    it('Should register failed with ivalid input', (done)=>{
-        // check by validator middleware
-        var account_invalid = {
-            fullname: "tester",
-            email: "tester@gmail.com",
-            password: "tester123456",
-            confirm_password: "tester123456-tester123456"
-        }
-        chai.request(server)
-        .post('/accounts/register')
-        .send(account_invalid)
-        .end((err,res) => {
-            expect(res.status).to.be.equal(400);   
-            expect(res.body).to.be.a('object');
-        done()
-        })
-    })
+    
 })
