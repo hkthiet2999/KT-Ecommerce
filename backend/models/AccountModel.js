bf90bebad1eac21cdbe526a11cc6597252ac7262
Kienn Thiett <smoothkt4951@gmail.com>
2021-07-11 14:05:02 +0700

    Add fields avatar, gender, birthday, address

--- backend/models/AccountModel.js
+++ backend/models/AccountModel.js
@@ -8,6 +8,23 @@ const AccountShema = new Schema({
         unique: true
     },
     password: String,
+    avatar: {
+        type: String,
+        default: "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+    },
+    address: {
+        type: String,
+        default: null
+    },
+    birthday: {
+        type: Date,
+        default: null
+    },
+    gender: {
+        type: String,
+        default: null
+    }
+
 })
 
 module.exports = mongoose.model('Account', AccountShema)
